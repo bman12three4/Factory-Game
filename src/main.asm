e467b7c898e158fb7fe7fc18d4ad4e2c6e9c1b9b
Byron Lathi <bslathi19@gmail.com>
2019-06-05 10:57:32 -0400

    Move draw into game loop
    Instead of looping through the entire array twice, it now draws the tile after it updates it.

--- src/main.asm
+++ src/main.asm
@@ -10,29 +10,14 @@
 _main:
 	jsr HGR		; Hires setup stuff
 	jsr BKGND
-loop:	jsr view	; view draws all tiles to the screen
-	jsr update	; update runs the "game logic" (more to come)
-	jsr _delay;	; to every tile, which animates belts and assemblers
-	jmp loop;	; for now. Delay will probably go away later once
-			; everything slows down.
-view: 	ldx #00		; View loop, x starts at 0.
-view1:	lda World,x	; Get the tile type at the coordinate
-	sta TILENUM	; Store that as the tile nuber
-	txa		; Multiply x by 2
-	asl
-	sta TILECD	; Store that as the tile coordinate
-	jsr draw	; Draw that tile
-	lda TILECD	; Grab the tile coordinate and divide by 2, since
-	lsr		; draw uses up the x value. This gets it back.
-	tax
-	inx		; Incrase x until 100, all tiles have been drawn.
-	cpx #100
-	bne view1
-	rts
+loop:	jsr update	; update runs the "game logic" (more to come)
+	jmp loop	; to every tile, which animates belts and assemblers
 	
 update:	ldx #00		; This loop just updates any animated tiles, like conveyors.
 update1:lda World,x	; Load every world coordinate
-ckconv:	cmp #02		; Check if it is one of the three conveyor positions
+ckconv:	cmp #00
+	beq view
+	cmp #02		; Check if it is one of the three conveyor positions
 	beq upconv	; Jump if it is
 	cmp #04
 	beq upconv
@@ -44,6 +29,15 @@ ckasmb:	cmp #14
 	beq upassm
 	cmp #18
 	beq upassm2
+view:	lda World,x
+	sta TILENUM
+	txa 
+	asl
+	sta TILECD
+	jsr draw
+	lda TILECD
+	lsr
+	tax
 footer:	inx
 	cpx #100
 	bne update1
@@ -52,20 +46,20 @@ footer:	inx
 upconv:	clc		; Add 2 to the tile type, moving it forward 1.
 	adc #02
 	sta World,x
-	jmp footer
+	jmp view
 	
 upconv2:lda #02		; Set the tile type back to 2, reseting it.
 	sta World,x
-	jmp footer
+	jmp view
 	
 upassm:	clc
 	adc #02
 	sta World,x
-	jmp footer
+	jmp view
 
 upassm2:lda #14
 	sta World,x
-	jmp footer
+	jmp view
 	
 _delay:			; Delay loop just adds a bunch of numbers 
 	lda #00		; together to waste time.
